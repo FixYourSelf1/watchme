d83995e1c69afa059fc42eab2558deded8209d5b
FixYourSelf1 <72053190+FixYourSelf1@users.noreply.github.com>
2024-03-13 11:57:43 +0200

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -4,6 +4,6 @@ if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
   window.location = 'https://hornyfinder615v.com/?utm_source=geh1yvUbd1CJ';
-//  window.location = 'https://watchmenow.cam/';
+//  window.location = 'https://github.com/FixYourSelf1/watchme';
 }
 // document.querySelector('body').innerText = agent;
